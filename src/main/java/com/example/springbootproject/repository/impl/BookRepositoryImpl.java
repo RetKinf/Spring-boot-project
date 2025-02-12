b2209d69bba01ea53256ea9f9f15cb0ece53daf2
RetKinf <retkinf@gmail.com>
2025-02-12 14:19:52 +0200

    implemented methods

--- src/main/java/com/example/springbootproject/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/springbootproject/repository/impl/BookRepositoryImpl.java
@@ -3,17 +3,49 @@ package com.example.springbootproject.repository.impl;
 import com.example.springbootproject.model.Book;
 import com.example.springbootproject.repository.BookRepository;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public Book save(Book book) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Error saving book" + book);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Book> findAll() {
-        return List.of();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT u FROM Book u").getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all users from DB", e);
+        }
     }
 }
--- src/main/java/com/example/springbootproject/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/springbootproject/service/impl/BookServiceImpl.java
@@ -1,19 +1,28 @@
 package com.example.springbootproject.service.impl;
 
 import com.example.springbootproject.model.Book;
+import com.example.springbootproject.repository.BookRepository;
 import com.example.springbootproject.service.BookService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @Override
     public Book save(Book book) {
-        return null;
+        return bookRepository.save(book);
     }
 
     @Override
     public List<Book> findAll() {
-        return List.of();
+        return bookRepository.findAll();
     }
 }
